eecda1682f5f9d02c2794470fa94d1a7fb90d186
hugosenari <hugosenari@gmail.com>
2022-03-12 00:10:23 -0300

    chore: re index nimdbx

---
+++ main/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Fast persistent key-value store, based on libmdbx'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimdbx-main.flake = false;
+  inputs.src-nimdbx-main.owner = "snej";
+  inputs.src-nimdbx-main.ref   = "refs/heads/main";
+  inputs.src-nimdbx-main.repo  = "nimdbx";
+  inputs.src-nimdbx-main.type  = "github";
+  
+  inputs."nimterop".dir   = "nimpkgs/n/nimterop";
+  inputs."nimterop".owner = "riinr";
+  inputs."nimterop".ref   = "flake-pinning";
+  inputs."nimterop".repo  = "flake-nimble";
+  inputs."nimterop".type  = "github";
+  inputs."nimterop".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimterop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-NimDBX-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-NimDBX-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
